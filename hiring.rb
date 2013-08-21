1ce44d9dad6521057abcb340d3fc2c1ff52c0574
Deepak Verma <deepakverma@Deepaks-MacBook-Pro.local>
2013-08-20 20:23:18 -0400

    Ruby Operator Exercise: Hiring a Programmer

---
+++ hiring.rb
@@ -0,0 +1,5 @@
+is_an_experienced_ruby_programmer =
+  (candidate.languages_worked_with.include?("Ruby")) &&
+  (candidate.years_of_experience >= 2 || candidate.github_points >= 500) &&
+  (candidate.age >= 15) && 
+  ! (candidate.applied_recently?)
